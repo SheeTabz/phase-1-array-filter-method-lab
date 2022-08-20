ec3e442216ba0408caa75df4853a2347377aa506
SheeTabz <wmuriithitabitha@gmail.com>
2022-08-20 14:21:59 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+// const num = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// console.log(num.filter(function(num){
+//     return (num > 5)
+// }))
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(arr, str){
+
+    return arr.filter(item => item==str)
+}
+
+function fuzzyMatch(arr, str){
+    return arr.filter((item)=>{
+        let word = item.split(',')
+        return word.find(i => i[0]==str.charAt(0))
+})
+}
+// const drivers = [
+//     {
+//       name: 'Bobby',
+//       hometown: 'Pittsburgh' },
+//     {
+//       name: 'Sammy',
+//       hometown: 'New York' } ,
+//     {
+//       name: 'Sally',
+//       hometown: 'Cleveland' },
+//     {
+//       name: 'Annette',
+//       hometown: 'Los Angeles' },
+//     {
+//       name: 'Bobby',
+//       hometown: 'Tampa Bay' }
+//   ];
+
+
+function matchName(arr, str){
+  return arr.filter(item => item.name === str);
+}
+console.log(matchName(drivers, "Bobby"))
